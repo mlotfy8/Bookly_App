5330fb7dab29ca047ba2aeec6f47eb6e37859782
mlotfy8 <mhmdlotfy450@gmail.com>
2023-09-11 03:12:57 +0300

    first commit

---
+++ lib/Screenes/Home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+
+    );
+  }
+}
